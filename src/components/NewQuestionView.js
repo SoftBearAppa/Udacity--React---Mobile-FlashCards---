b5074b7e5277b2f3dfd47f2282d884586d182d1b
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-07 22:46:29 -0900

    Started to add supporting components & state to input question/answer

--- src/components/NewQuestionView.js
+++ src/components/NewQuestionView.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, KeyboardAvoidingView } from 'react-native';
 
 class NewQuestionView extends Component {
+  state = {
+    question: '',
+    answer: '',
+  }
+
   render() {
     return (
       <View>
