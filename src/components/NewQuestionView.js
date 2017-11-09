57a5ec83a059e7833672e66d081e9274d2764bee
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-08 15:07:10 -0900

    Added a managed TextInput for the 'question' & 'answer'

--- src/components/NewQuestionView.js
+++ src/components/NewQuestionView.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Text, View, TextInput, KeyboardAvoidingView } from 'react-native';
+import { Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 
 class NewQuestionView extends Component {
   state = {
@@ -9,9 +9,19 @@ class NewQuestionView extends Component {
   }
 
   render() {
+    const { question, answer} = this.state;
     return (
       <View>
         <Text>New Question</Text>
+        <KeyboardAvoidingView>
+          <Text>Question:</Text>
+          <TextInput value={question} onChangeText={question => this.setState({question})} placeholder={'Enter Question'} />
+          <Text>Answer:</Text>
+          <TextInput value={answer} onChangeText={answer => this.setState({answer})} placeholder={'Enter Answer to Question'} />
+          <TouchableOpacity onPress={() => console.log(this.state)}>
+            <Text>Submitt</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
       </View>
     )
   }
