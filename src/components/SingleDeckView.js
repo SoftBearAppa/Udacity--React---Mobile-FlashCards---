6d62b1e5e3bf844f2a69f1a71449a3e2ab019311
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-15 14:47:57 -0900

    Added 'Animated.timing' to main view when 'SingleDeckView' loads, per requirments

--- src/components/SingleDeckView.js
+++ src/components/SingleDeckView.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Animated } from 'react-native';
 
 class SingleDeckView extends Component {
+  state = {
+    timing: new Animated.Value(0)
+  }
+
+  componentDidMount() {
+    const { timing } = this.state;
+    Animated.timing(
+      timing, {
+        toValue: 1,
+        duration: 1000,
+      }
+    ).start();
+  }
+
   render() {
     console.log(this.props);
+    const { timing } = this.state;
     const { navigate } = this.props.navigation;
     const { title } = this.props.navigation.state.params;
     const questions = this.props.decks[title].questions;
     return (
-      <View>
+      <Animated.View style={{opacity: timing}} >
         <Text>{title}</Text>
         <Text>Cards: {questions.length}</Text>
         <TouchableOpacity onPress={() => navigate('NewQuestionView', {title, questions})} ><Text>Add Question</Text></TouchableOpacity>
         <TouchableOpacity onPress={() => navigate('QuizView', { title, questions })} ><Text>Start Quiz</Text></TouchableOpacity>
-      </View>
+      </Animated.View>
     )
   }
 }
